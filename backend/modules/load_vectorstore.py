4748a678b3b294904197250ba0de186da978e138
uyaqoob700-gif <uyaqoob700@gmail.com>
2025-11-12 12:39:17 +0500

    Update load_vectorstore.py

--- backend/modules/load_vectorstore.py
+++ backend/modules/load_vectorstore.py
@@ -6,7 +6,7 @@ from tqdm.auto import tqdm
 from pinecone import Pinecone, ServerlessSpec
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings  import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 # Load environment variables
 load_dotenv()
